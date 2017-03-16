7db49be2c42517cac654db0050afb1123cc64644
Ciaran Kelly <ciaran.kelly@gmail.com>
2017-03-16 14:54:15 +0000

    Implementing Matcher

---
+++ spec/MatcherSpec.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace spec\DataValidata\FuzzyStrings;
+
+use DataValidata\FuzzyStrings\Matcher;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class MatcherSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Matcher::class);
+    }
+
+    function it_does_exact_matching_by_default()
+    {
+        $this->matches("needle", "needle")->shouldBe(true);
+        $this->matches("needle", "NEEDLE")->shouldBe(false);
+    }
+
+    function it_allows_case_insensitive_matching()
+    {
+        $this->caseInsensitive()->matches("needle", "needle")->shouldBe(true);
+        $this->caseInsensitive()->matches("needle", "NEEDLE")->shouldBe(true);
+    }
+
+    function it_allows_a_threshold_to_be_declared()
+    {
+        $this->matches("NeEdLE", "can you find the need in this haystack?", 3)->shouldBe(false);
+        $this->matches("NeEdLE", "can you find the needle in this haystack?", 4)->shouldBe(true);
+
+        $this->caseInsensitive()->matches("needle", "can you find the need in this haystack?", 1)->shouldBe(false);
+        $this->caseInsensitive()->matches("NeEdLE", "can you find the needle in this haystack?", 1)->shouldBe(true);
+    }
+}
---
+++ src/Matcher.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace DataValidata\FuzzyStrings;
+
+class Matcher
+{
+    private $caseSensitive = true;
+
+    public function caseInsensitive()
+    {
+        $newInstance = new static();
+        $newInstance->caseSensitive = false;
+        return $newInstance;
+    }
+
+    public function matches($needle, $haystack, $threshold = 0)
+    {
+        return $this->minimumEditsRequired($needle, $haystack) <= $threshold;
+    }
+
+    /**
+     * @param $needle
+     * @param $haystack
+     * @return mixed
+     */
+    private function minimumEditsRequired($needle, $haystack)
+    {
+        $needleLength   = strlen($needle);
+        $haystackLength = strlen($haystack);
+
+        // short circuit best case scenarios
+        if($needleLength == 1) {
+            /**
+             * needle is only one character long, so, either it is in the haystack,
+             * meaning no edits are required, or, it is not, meaning a single edit
+             * would be required.
+             */
+            if(!$this->caseSensitive) {
+                $needle = strtolower($needle);
+                $haystack = strtolower($haystack);
+            }
+            return (int) !(strstr($haystack, $needle));
+        }
+
+        if(!$haystackLength) {
+            /**
+             * an empty haystack will require needleLength edits to match
+             */
+            return $needleLength;
+        }
+
+        return min($this->calculateBottomRowOfLevenshteinTable($needle, $haystack));
+    }
+
+    /**
+     * Algorithm ported from:
+     *      http://ginstrom.com/scribbles/2007/12/01/fuzzy-substring-matching-with-levenshtein-distance-in-python/
+     * @param $needle
+     * @param $haystack
+     * @return array
+     */
+    private function calculateBottomRowOfLevenshteinTable($needle, $haystack)
+    {
+        if(!$this->caseSensitive) {
+            $needle = strtolower($needle);
+            $haystack = strtolower($haystack);
+        }
+
+        $m = strlen($needle);
+        $n = strlen($haystack);
+        $row1 = array_fill(0, $n + 2, 0);  // initialise first row to zeros
+        foreach (range(0, $m - 1) as $i) {
+            $row2 = [$i + 1];
+            foreach (range(0, $n - 1) as $j) {
+                $cost = ($needle{$i} != $haystack{$j});
+
+                $possibleEdits = [
+                    $row1[$j + 1] + 1,     // deletion
+                    $row2[$j] + 1,         // insertion
+                    $row1[$j] + $cost,     // substitution
+                ];
+                array_push($row2, min($possibleEdits));
+            }
+            $row1 = $row2;
+        }
+        return $row1;
+    }
+}
